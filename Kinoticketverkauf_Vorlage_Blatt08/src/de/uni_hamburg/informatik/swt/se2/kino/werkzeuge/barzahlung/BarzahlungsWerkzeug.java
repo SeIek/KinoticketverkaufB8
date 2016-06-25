b914689a61d90669b5fdf16932af035e4161b683
SeIek <5sandmei@informatik.uni-hamburg.de>
2016-06-25 22:48:03 +0200

    Linas Änderungen

--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarzahlungsWerkzeug.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarzahlungsWerkzeug.java
@@ -1,9 +1,153 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.barzahlung;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+/**
+ * 
+ * Das Barzahlungswerkzeug. Mit diesem Werkzeug kann der Benutzer eine Barzahlung durchführen oder abbrechen.
+ * 
+ * @author Lina Heinzke, SE2 Übungsgruppe "No Pascha"
+ * @version 25.06.2016
+ *
+ */
 public class BarzahlungsWerkzeug
 {
+    // Die UI dieses Werkzeugs. 
+    private BarzahlungsWerkzeugUI _ui;
+
+    // Der zu zahlende Betrag
+    private int _preis;
+
+    // Wahrheitswert, ob mind. der geforderte Betrag gezahlt (ins JTextField eingegeben) wurde.
+    private boolean _wurdeBezahlt;
+
+    /**
+     * Führt die Barzahlung durch.
+     * 
+     * @param betrag Der zu zahlende Betrag.
+     * @return  true, wenn auf "OK" geklickt wurde
+     *          false, wenn auf "Abbrechen geklickt wurde"
+     * 
+     * @require betrag >= 0
+     */
     public boolean zahlen(int betrag)
     {
+        assert betrag >= 0 : "Vorbedingung verletzt: betrag >= 0";
+        
+        _preis = betrag;
+
+        _ui = new BarzahlungsWerkzeugUI(new JFrame(), betrag);
+
+        registriereUIAktionen();
+
+        _ui.zeigeFenster();
+
+        return _wurdeBezahlt;
+    }
+    
+    /**
+     * Fügt der UI die Funktionalität hinzu mit entsprechenden Listenern.
+     */
+    private void registriereUIAktionen()
+    {
+        _ui.getCancelButton()
+        .addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                reagiereAufBeendenButton();
+            }
+        });
+        
+        _ui.getOKButton()
+        .addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                reagiereAufOKButton();
+            }
+        });
+        
+        _ui.getEingabefeld()
+        .addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                reagiereAufEingabe();
+            }
+        });
+    }
+
+    /**
+     * Schließt das Fenster.
+     */
+    private void reagiereAufBeendenButton()
+    {
+        _wurdeBezahlt = false;
+        _ui.schliesseFenster();
+    }
+
+    /**
+     * Vermerkt, dass bezahlt wurde und schließt das Fenster.
+     */
+    private void reagiereAufOKButton()
+    {
+        _wurdeBezahlt = true;
+        _ui.schliesseFenster();
+    }
+
+    /**
+     * Aktiviert den OKButton, wenn genug gezahlt wurde und setzt den Restbetrag.
+     */
+    private void reagiereAufEingabe()
+    {
+        int gezahlt = getBetrag(_ui.getEingabefeld());
+        _ui.getOKButton()
+            .setEnabled(gezahlt >= _preis);
+        _ui.setRestbetrag(gezahlt - _preis);
+    }
+
+    //TODO sinnvoller in der UI Klasse? 
+    /**
+     * Gibt dir Zahl zurück, der in einem JTextField steht (wenn keine Zahl 0).
+     * 
+     * @param textField Das JTextField, aus dem die Zahl berechnet werden soll.
+     * @return Zahl im TextField; wenn keine Zahl 0
+     */
+    private int getBetrag(JTextField textField)
+    {
+        String text = textField.getText();
+        if (istInt(text))
+        {
+            return Integer.parseInt(text);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    /**
+     * Gibt zurück, ob ein String nur aus Ziffern besteht.
+     * @param text Der zu prüfende String
+     * @return Gibt zurück, ob der String eine Zahl ist.
+     */
+    public static boolean istInt(String text)
+    {
+        for (int i = 0; i < text.length(); i++)
+        {
+            if (!Character.isDigit(text.charAt(i)))
+            {
+                return false;
+            }
+        }
         return true;
     }
 }
--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarzahlungsWerkzeugUI.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarzahlungsWerkzeugUI.java
@@ -16,146 +16,151 @@ import javax.swing.SwingConstants;
 /**
  * Das UI für das BarzahlungsWerkzeug.
  * 
- * @version 25.06.2016
+ * @version 24.06.2016
  */
-class BarzahlungsWerkzeugUI
-{
-    private JDialog _dialogfenster;
-    private JFrame _fenster_hauptprogramm;
+public class BarzahlungsWerkzeugUI
+{    
+        private JDialog _dialogfenster;
+        private JFrame _fenster_hauptprogramm;        
 
-    private JPanel _hauptfenster;
-    private JPanel _behilfspanel;
+        private JPanel _hauptfenster;
+        private JPanel _behilfsframe;
 
-    private JPanel _buttonpanel;
-    private JButton _button_ok;
-    private JButton _button_abbrechen;
+        private JPanel _buttonpanel;
+        private JButton _button_ok;
+        private JButton _button_abbrechen;
 
-    private final String TITEL = "Barzahlung";
+        private static final String TITEL = "Barzahlung";
 
-    private final String ZUZAHLEN = "zu zahlen:";
-    private String _zuzahlen_betrag;
+        private static final String ZUZAHLEN = "zu zahlen:";
+        private String _zuzahlen_betrag;
+        
 
-    private final String GEZAHLT = "gezahlt:";
-    private JTextField _eingabebetrag;
+        private static final String GEZAHLT = "gezahlt:";
+        private JTextField _eingabebetrag;
 
-    private final String RESTBETRAG = "Restbetrag:";
-    private String _restbetrag;
+        private static final String RESTBETRAG = "Restbetrag:";
+        private String _restbetrag_betrag;
+     // TODO Hier schon JLabel bennenen, damit darauf zugegriffen werden kann
+        private JLabel _RestbetragLabel;
 
-    private final String BESTAETIGEN = "OK";
-    private final String ABBRECHEN = "Abbrechen";
+        private static final String BESTAETIGEN = "OK";
+        private static final String ABBRECHEN = "Abbrechen";
 
-    /**
-     * Der komplette JDialog wird im Konstruktor... konstruiert.
-     * 
-     * @param fenster_hauptprogramm Der Parent JFrame des JDialog, von dem dieser abhängt.
-     * @param zuzahlen Der zu zahlende Geldbetrag, der angezeigt wird.
-     */
-    public BarzahlungsWerkzeugUI(JFrame fenster_hauptprogramm, int zuzahlen)
-    {
-        assert fenster_hauptprogramm != null : "Vorbedingung verletzt: fenster_hauptprogramm != null";
-        assert zuzahlen >= 0 : "Vorbedingung verletzt: zuzahlen >= 0";
+        /**
+         * Der komplette JDialog wird im Konstruktor... konstruiert.
+         * 
+         * @param fenster_hauptprogramm Der Parent JFrame des JDialog, von dem dieser abhängt.
+         * @param zuzahlen Der zu zahlende Geldbetrag, der angezeigt wird.
+         */
+        public BarzahlungsWerkzeugUI(JFrame fenster_hauptprogramm, int zuzahlen)
+        {
+            assert fenster_hauptprogramm != null : "Vorbedingung verletzt: fenster_hauptprogramm != null";
+            assert zuzahlen >= 0 : "Vorbedingung verletzt: zuzahlen >= 0";            
+            
+            _fenster_hauptprogramm = fenster_hauptprogramm;
+            
+            _dialogfenster = new JDialog(_fenster_hauptprogramm, TITEL, true);
+            erstelleHauptfenster(zuzahlen);
+            _dialogfenster.getContentPane().add(_hauptfenster, BorderLayout.NORTH);
+            
+            _dialogfenster.add(new JSeparator());
+            
+            erstelleButtonPanel();
+            _dialogfenster.getContentPane().add(_buttonpanel, BorderLayout.SOUTH);
 
-        _fenster_hauptprogramm = fenster_hauptprogramm;
+            _dialogfenster.pack();
+            _dialogfenster.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE); // oder JDialog.HIDE_ON_CLOSE ?
+            _dialogfenster.setLocationRelativeTo(null); // zentriert den Dialog in der Bildschirmmitte
+            //TODO Sichtbarkeit erst nach registierung sinnvoll
+            //_dialogfenster.setVisible(true);
+        }
 
-        _dialogfenster = new JDialog(_fenster_hauptprogramm, TITEL, true);
-        erstelleHauptfenster(zuzahlen);
-        _dialogfenster.getContentPane()
-            .add(_hauptfenster, BorderLayout.NORTH);
+        /**
+         * Erstellt das Hauptfenster mit den Indizes, Beträgen und dem Eingabefeld.
+         * 
+         * @param zuzahlen Der zu zahlende Geldbetrag, der angezeigt wird.
+         */
+        private void erstelleHauptfenster(int zuzahlen)
+        {
+            _hauptfenster = new JPanel();
+            GridLayout tabelle_layout = new GridLayout(0, 2, 0, 0);
+            _hauptfenster.setLayout(tabelle_layout);
+            
+            _behilfsframe = new JPanel();// ohne diesen bringt das JTextfield die Reihenfolge im GridLayout durcheinander
 
-        _dialogfenster.add(new JSeparator());
+            _zuzahlen_betrag = "" + zuzahlen;
+            _eingabebetrag = new JTextField(10); 
+            _eingabebetrag.setHorizontalAlignment(JTextField.CENTER);
+            _restbetrag_betrag = "0"; // zeigt anfangs den Wert 0 an, wird später durch das BarzahlungsWerkzeug berechnet.
 
-        erstelleButtonPanel();
-        _dialogfenster.getContentPane()
-            .add(_buttonpanel, BorderLayout.SOUTH);
+            // TODO JLabel erzeugen und zuweisen
+            _RestbetragLabel = new JLabel(_restbetrag_betrag, SwingConstants.CENTER);
+            _behilfsframe.add(_eingabebetrag, SwingConstants.CENTER);
+            _hauptfenster.add(new JLabel(ZUZAHLEN, SwingConstants.CENTER));
+            _hauptfenster.add(new JLabel(_zuzahlen_betrag, SwingConstants.CENTER));
+            _hauptfenster.add(new JLabel(GEZAHLT, SwingConstants.CENTER));
+            _hauptfenster.add(_behilfsframe);
+            _hauptfenster.add(new JLabel(RESTBETRAG, SwingConstants.CENTER));
+            _hauptfenster.add(_RestbetragLabel);
+        }
 
-        _dialogfenster.pack();
-        _dialogfenster.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE); // oder JDialog.HIDE_ON_CLOSE ?
-        _dialogfenster.setLocationRelativeTo(null); // zentriert den Dialog in der Bildschirmmitte
-        _dialogfenster.setVisible(true);
-    }
+        /**
+         * Erstellt das Panel mit den beiden Buttons.
+         */
+        private void erstelleButtonPanel()
+        {
+            _buttonpanel = new JPanel();
+            FlowLayout buttonpanel_layout = new FlowLayout();
+            _buttonpanel.setLayout(buttonpanel_layout);
 
-    /**
-     * Erstellt das Hauptfenster mit den Indizes, Beträgen und dem Eingabefeld.
-     * 
-     * @param zuzahlen Der zu zahlende Geldbetrag, der angezeigt wird.
-     */
-    private void erstelleHauptfenster(int zuzahlen)
-    {
-        _hauptfenster = new JPanel();
-        GridLayout tabelle_layout = new GridLayout(0, 2, 0, 0);
-        _hauptfenster.setLayout(tabelle_layout);
+            _button_ok = new JButton(BESTAETIGEN);
+            // TODO Soll am Anfang nicht angeklickt werden können -> false 
+            _button_ok.setEnabled(false);
+            _buttonpanel.add(_button_ok);
 
-        _behilfspanel = new JPanel();// ohne diesen bringt das JTextfield die Reihenfolge im GridLayout durcheinander.
+            _button_abbrechen = new JButton(ABBRECHEN);
+            _buttonpanel.add(_button_abbrechen);
+        }
 
-        _zuzahlen_betrag = "" + zuzahlen;
-        _eingabebetrag = new JTextField(10);
-        _eingabebetrag.setHorizontalAlignment(JTextField.CENTER);
-        _restbetrag = "0"; // zeigt anfangs den Wert 0 an, wird später durch das BarzahlungsWerkzeug berechnet.
+        /**
+         * Zeigt das Fenster an.
+         */
+        public void zeigeFenster()
+        {
+            _dialogfenster.setVisible(true);
+        }
 
-        _behilfspanel.add(_eingabebetrag, SwingConstants.CENTER);
-        _hauptfenster.add(new JLabel(ZUZAHLEN, SwingConstants.CENTER));
-        _hauptfenster.add(new JLabel(_zuzahlen_betrag, SwingConstants.CENTER));
-        _hauptfenster.add(new JLabel(GEZAHLT, SwingConstants.CENTER));
-        _hauptfenster.add(_behilfspanel);
-        _hauptfenster.add(new JLabel(RESTBETRAG, SwingConstants.CENTER));
-        _hauptfenster
-            .add(new JLabel(_restbetrag, SwingConstants.CENTER));
-    }
+        /**
+         * Schließt das Fenster.
+         */
+        public void schliesseFenster()
+        {
+            _dialogfenster.dispose(); // oder _dialogfenster.setVisible(false); ?
+        }
+        
+        public JButton getOKButton()
+        {
+            return _button_ok;
+        }
 
-    /**
-     * Erstellt das Panel mit den beiden Buttons.
-     */
-    private void erstelleButtonPanel()
-    {
-        _buttonpanel = new JPanel();
-        FlowLayout buttonpanel_layout = new FlowLayout();
-        _buttonpanel.setLayout(buttonpanel_layout);
-
-        _button_ok = new JButton(BESTAETIGEN);
-        _buttonpanel.add(_button_ok);
-
-        _button_abbrechen = new JButton(ABBRECHEN);
-        _buttonpanel.add(_button_abbrechen);
-    }
-
-    /**
-     * Zeigt das Fenster an.
-     */
-    public void zeigeFenster()
-    {
-        _dialogfenster.setVisible(true);
-    }
-
-    /**
-     * Schließt das Fenster.
-     */
-    public void schliesseFenster()
-    {
-        _dialogfenster.dispose(); // oder _dialogfenster.setVisible(false); ?
-    }
-
-    public JButton getOKButton()
-    {
-        return _button_ok;
-    }
-
-    public JButton getCancelButton()
-    {
-        return _button_abbrechen;
-    }
-
-    public JTextField getEingabefeld()
-    {
-        return _eingabebetrag;
-    }
-    
-    public String getZuzahlenBetrag()
-    {
-        return _zuzahlen_betrag;
-    }
-    
-    public String getRestbetrag()
-    {
-        return _restbetrag;
-    }
+        public JButton getCancelButton()
+        {
+            return _button_abbrechen;
+        }
+        
+        public JTextField getEingabefeld()
+        {
+            return _eingabebetrag;
+        }
+        
+        //TODO Methode zum setzen des Restbetrags
+        /**
+         * Setzt den Text im JLabel _RestbetragLabel auf den Restbetrag
+         * @param betrag Der Restbetrag
+         */
+        public void setRestbetrag(int betrag)
+        {
+            _RestbetragLabel.setText("" + betrag);
+        }
 }
